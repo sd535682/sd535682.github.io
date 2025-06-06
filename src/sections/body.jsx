f9da00d51230d58659fa264be079c586f33575a4
sd535682 <sd535682@gmail.com>
2025-06-06 17:14:55 +0530

    add exp comp

--- src/sections/body.jsx
+++ src/sections/body.jsx
@@ -1,5 +1,6 @@
 import ContactForm from "../components/contactform";
 import Education from "../components/education";
+import Experience from "../components/experience";
 import Projects from "../components/projects";
 import Skills from "../components/skills";
 import Profile from "../components/userinfo";
@@ -8,8 +9,9 @@ export default function Body() {
   return (
     <div className="flex flex-col gap-5 max-w-[576px]">
       <Profile />
-      <Skills />
+      <Experience />
       <Projects />
+      <Skills />
       <Education />
       <ContactForm />
     </div>
